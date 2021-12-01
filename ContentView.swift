271382c2f5ff1e636eee4e0be8089842163069dc
Aleksandr <Shlepkinaleksandr@mail.ru>
2021-12-01 16:51:47 +0300

    Light Animation

--- ContentView.swift
+++ ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     
     @State private var login: String = ""
     @State private var password: String = ""
+    @State private var scale: CGFloat = 50
+    @State private var shoudShowLogo: Bool = true
+    
+    private let keyboard = Publishers.Merge(
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .map { _ in true },
+        NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ in false }
+    ).removeDuplicates()
     
     var body: some View {
         ZStack {
@@ -19,11 +29,25 @@ struct ContentView: View {
                 
         VStack {
         ScrollView {
+         
             VStack{
-                Image("icons8-вконтакте-128")
-                    .frame(width: 120, height: 120, alignment: .center)
-                    .padding(.top, 130 )
-                
+                if shoudShowLogo == true {
+                Image("VKLog")
+                    .padding(.top, 120 )
+                    .scaleEffect(scale)
+                    .onAppear {
+                        let baseAnimation = Animation.easeOut(duration: 1.0)
+                        return withAnimation(baseAnimation) {
+                            self.scale = 1
+                        }
+                    }
+                } else {
+                    Image("VKLog")
+                        .resizable()
+                        .frame(width: 30, height: 30, alignment: .center)
+
+                    
+                }
                 VStack(){
                     HStack{
                         Text ("Login")
@@ -36,25 +60,36 @@ struct ContentView: View {
                         TextField("Password", text: $password).frame(width: 150, height: 30)
                     }
                 }.frame(maxWidth: 250)
+                .padding()
             }
             VStack {
                 Button(action: { print ("Test")}) {
                     Text("Sing Up").foregroundColor(.white).frame(width: UIScreen.main.bounds.width - 260).padding()}.background(Color.lightBlue).clipShape(Capsule())
                     .padding(.top, 32)
+                    .disabled(login.isEmpty || password.isEmpty)
             }
             
             VStack{
                 HStack{
                     Button(action: { }) {
                         Text("Don't have account?").foregroundColor(.secondary).padding()
-                    }.disabled(login.isEmpty || password.isEmpty)
+                    }.disabled(true)
                 }
             }
             
         }.textFieldStyle(RoundedBorderTextFieldStyle())
+        .onReceive(keyboard) { value in
+            self.shoudShowLogo = !value
             
+        }.onTapGesture {
+            self.endEditing()
+        }
+        
     }
     }
+    }
+    func endEditing(){
+        UIApplication.shared.endEditing()
     }
     
     struct ContentView_Previews: PreviewProvider {
@@ -66,3 +101,8 @@ struct ContentView: View {
 
 
 
+extension UIApplication {
+    func endEditing() {
+        self.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
